3fe563765c48007bad465d55e478acbd78ab48c9
Tatsuro <tatsurow1122@gmail.com>
2020-05-04 20:05:18 +0900

    Add validation to Comment model

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = @book.id
-    comment.save
-    redirect_to book_path(@book)
+    @created_book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @created_book.id
+    if @book_comment.save
+      redirect_to book_path(@created_book)
+    else
+      @book = Book.new
+      @post_book = Book.find(params[:book_id])
+      render 'books/show'
+    end
   end
 
   def destroy
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,6 @@
 class FavoritesController < ApplicationController
+  before_action :baria_favorite
+
   def create
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: params[:book_id])
@@ -6,11 +8,16 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: params[:book_id])
+    favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     favorite.destroy
   end
 
   private
-
+  def baria_favorite
+    if favorite.present?
+      favorite = Favorite .find_by(book_id: book.id, user_id: current_user)
+    end
+  end
 end
